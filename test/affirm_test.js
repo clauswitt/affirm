a67f758dbb0b7d2787a1e94710983de9e98a5b60
Claus Witt <claus@wittnezz.dk>
2012-09-04 08:59:56 +0200

    added some more tests

--- test/affirm_test.js
+++ test/affirm_test.js
@@ -20,16 +20,22 @@ var affirm = require('../lib/affirm.js');
     test.ifError(value)
 */
 
-exports['awesome'] = {
+exports['affirm'] = {
   setUp: function(done) {
     // setup here
     done();
   },
   'no args': function(test) {
-    test.expect(2);
+    test.expect(4);
     // tests here
     test.equal(affirm.assertEquals(1,2), false, 'one does not equal two');
     test.equal(affirm.assertEquals(2,2), true, 'two does equal two');
+
+    var array1 = [1, 2];
+    var array2 = [1, 2];
+    var array3 = array1;
+    test.equal(affirm.assertIdentical(array1, array2), false, 'Not identical');
+    test.equal(affirm.assertIdentical(array1, array3), true, 'Identical');
     test.done();
   }
 };
